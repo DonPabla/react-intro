f918e095e53d1f0f0886c8092b3b03d8178df17a
DonPabla <zaderzedar@gmail.com>
2023-01-12 10:10:16 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="output">
+          <input type="text" defaultValue="0" />
+        </div>
+        <div className="buttons">
+          <button>1</button>
+        </div>
+      </div>
+    )
+
+  }
+
+}
+
+export default App
